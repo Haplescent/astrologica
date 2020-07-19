b4aa19c62417b62a6ef4f42baafbffd68afd7901
John Merritt <johnmerritt95@gmail.com>
2020-07-19 16:43:07 -0700

    make it better

---
+++ public/js/members.js
@@ -0,0 +1,115 @@
+$(document).ready(function () {
+  // mobile responive nav for materialize
+  $(".sidenav").sidenav();
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function (data) {
+    // personal greeting
+    const name = data.firstname;
+
+    $(".nav-wrapper").append(
+      ` <a href="" id="brand-logo">Welcome ${name}!</a>`
+    );
+
+    // formatting birthday
+    console.log(data.birthday);
+
+    const birthday = data.birthday;
+    const birthdayFormatted = moment(birthday).add(1, "days");
+    const bdayMonth = moment(birthdayFormatted).format("MM");
+    const bdayDay = moment(birthdayFormatted).format("DD");
+
+    console.log(bdayMonth);
+    console.log(bdayDay);
+
+    // zodiac function
+    function zodiac(day, month) {
+      const zodiac = [
+        "",
+        "Capricorn",
+        "Aquarius",
+        "Pisces",
+        "Aries",
+        "Taurus",
+        "Gemini",
+        "Cancer",
+        "Leo",
+        "Virgo",
+        "Libra",
+        "Scorpio",
+        "Sagittarius",
+        "Capricorn",
+      ];
+      const lastDay = ["", 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+      return day > lastDay[month] ? zodiac[month * 1 + 1] : zodiac[month];
+    }
+    const sign = zodiac(parseInt(bdayDay), parseInt(bdayMonth));
+    const signLowerCase = sign.toLowerCase();
+
+    console.log(signLowerCase);
+
+    // API call
+    $.ajax({
+      type: "GET",
+      url: `http://sandipbgt.com/theastrologer/api/horoscope/${signLowerCase}/today`,
+      dataType: "json",
+    }).then((res) => {
+      console.log(res);
+
+      const horoscope = res.horoscope;
+      const horoscopeEdit = horoscope.slice(0, -59);
+
+      $("#horoscope").append(`
+              <div class="card">
+                <div class="front-card">
+                  <div class="horoscope">
+                    <img src="./images/${signLowerCase}.png">
+                  </div>
+                  <div class="bar">
+                    <h4>${sign}</h4>
+                  </div>
+                </div>
+                <div class="back-card white-text">
+                <h5>Today's Horoscope</h5>
+                  <p id="horoscopeParagraph">${horoscopeEdit}</p>
+                </div>
+              </div>
+            `);
+    });
+
+    console.log(name);
+  });
+
+  $.get(`/api/profilepic`).then((res) => {
+    console.log(res);
+    console.log(res.image);
+
+    if (res.image) {
+      $("#dashboardprofile").append(`
+        <a  href="/profile">
+          <img src="../uploads/${res.image}" 
+            style=" vertical-align: middle;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;" 
+            alt="Avatar" 
+            class="avatar">
+      </a>`);
+    } else {
+      $("#dashboardprofile").append(`
+        <a  href="/profile">
+        <img
+        src="./images/Profile.png"
+        style="
+          vertical-align: middle;
+          width: 70px;
+          height: 50px;
+          border-radius: 50%;
+        "
+        alt="Avatar"
+        class="avatar"
+    />
+      </a>`);
+    }
+  });
+});
---
+++ public/js/signup.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  // mobile responive nav for materialize
+  $(".sidenav").sidenav();
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+  var birthdayInput = $("#birthday-input");
+  var firstnameInput = $("#firstname-input");
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function (event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+      birthday: birthdayInput.val().trim(),
+      firstname: firstnameInput.val().trim(),
+    };
+
+    if (
+      !userData.email ||
+      !userData.password ||
+      !userData.birthday ||
+      !userData.firstname
+    ) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(
+      userData.email,
+      userData.password,
+      userData.birthday,
+      userData.firstname
+    );
+    emailInput.val("");
+    passwordInput.val("");
+    birthdayInput.val("");
+    firstnameInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(email, password, birthday, firstname) {
+    $.post("/api/signup", {
+      email: email,
+      password: password,
+      birthday: birthday,
+      firstname: firstname
+    })
+      .then(function (data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
