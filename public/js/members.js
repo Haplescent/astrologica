56c4dd55e518b879f9befdb6cb2c68e49111933e
John Merritt <johnmerritt95@gmail.com>
2020-07-19 17:14:37 -0700

    testing out http resquest

--- public/js/members.js
+++ public/js/members.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
     // API call
     $.ajax({
       type: "GET",
-      url: `sandipbgt.com/theastrologer/api/horoscope/${signLowerCase}/today`,
+      url: `www.sandipbgt.com/theastrologer/api/horoscope/${signLowerCase}/today`,
       dataType: "json",
     }).then((res) => {
       console.log(res);
